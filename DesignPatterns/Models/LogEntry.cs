cd60b37270f71de24827966868daad0f4ed72fdb
Dmitry Vasilyuk <dmitriy.vasilyuk@fortegrp.net>
2019-01-25 13:12:36 +0300

    Added some data

---
+++ DesignPatterns/Models/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Models
+{
+    public class LogEntry
+    {
+        public string Message { get; set; }
+        public LogType LogType { get; set; }
+    }
+
+    public enum LogType
+    {
+        Warning,
+        Error,
+        Info
+    }
+}
---
+++ DesignPatterns/Storage/DBStorage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using DesignPatterns.Models;
+
+namespace DesignPatterns.Storage
+{
+    public class DbStorage
+    {
+        public IEnumerable<LogEntry> LogEntries => new List<LogEntry>()
+        {
+            new LogEntry { Message = "Something went wrong!", LogType = LogType.Error },
+            new LogEntry { Message = "All are working good now.", LogType = LogType.Info },
+            new LogEntry { Message = "Something bad happens! Error 404", LogType = LogType.Error },
+            new LogEntry { Message = "Please be careful", LogType = LogType.Warning }
+        };
+    }
+}
